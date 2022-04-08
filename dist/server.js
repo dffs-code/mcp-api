5fe0b1769fb40b592fb5a0c45af29a899ee9ff89
Daniel <d.soares@aluno.ifsp.edu.br>
2022-04-08 04:17:12 -0300

    atualiza porta

--- dist/server.js
+++ dist/server.js
@@ -1,3 +1,3 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
+'use strict'; function _interopRequireDefault (obj) { return obj && obj.__esModule ? obj : { default: obj } } const _app = require('./app'); const _app2 = _interopRequireDefault(_app)
 
-_app2.default.listen(3333)
+_app2.default.listen(process.env.PORT)
